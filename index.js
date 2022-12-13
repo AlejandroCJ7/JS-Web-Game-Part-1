88ffb75db72c834574b820e1b2e7c000ae9305ab
Alex Castaneda <alex@Alexs-MacBook-Pro.local>
2022-12-13 13:43:07 -0800

    JS Web Game Updated

--- index.js
+++ index.js
@@ -1,6 +1,43 @@
-let greenCharacter = document.createElement('img')
-greenCharacter.src = 'assets/green-character.gif'
-greenCharacter.style.position = 'fixed'
-greenCharacter.style.left = '100px'
-greenCharacter.style.bottom = '100px'
-document.body.append(greenCharacter)
+
+
+function newImage(src, left, bottom) {
+    let imgElement = document.createElement('img');
+    imgElement.src = src;
+    imgElement.style.position = 'fixed';
+    imgElement.style.left = left;
+    imgElement.style.bottom = bottom;
+    document.body.append(imgElement);
+}
+  newImage('assets/green-character.gif', '100px', '100px');
+  newImage('assets/pine-tree.png', '450px', '200px'); 
+  newImage('assets/tree.png', '200px', '300px');
+  newImage('assets/pillar.png', '350px', '100px');
+  newImage('assets/crate.png', '150px', '200px');
+  newImage('assets/well.png', '500px', '425px');
+
+  function newItem(src, left, bottom) {
+  let sword = document.createElement('img');
+  sword.src = src;
+  sword.style.position = 'fixed';
+  sword.style.left = left;
+  sword.style.bottom = bottom;
+  document.body.append(sword); 
+  
+  sword.addEventListener('click', function() {
+    sword.remove()
+})
+  }
+  newItem('assets/sword.png', '500px', '405px');
+  newItem('assets/sheild.png', '165px', '185px');
+  newItem('assets/staff.png', '600px', '100px');
+   
+  /* --this is my attempt at not trying to use document.createElement twice but i couldnt figure it out and tried looking up videos but nothing worked----
+  
+  function newItem(src, left, bottom) {
+let sword = newItem(src, left, bottom)
+ 
+sword.addEventListener('click', function() {
+      sword.remove()
+  })
+} */
+
